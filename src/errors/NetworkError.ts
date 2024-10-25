15c851c69f6f837a8817894ffd2d4b3f203311b1
hyogeun <stonepagoda8@gmail.com>
2024-10-25 18:42:55 +0900

    feat: 422 에러 메시지 추가
    - 422 에러 메시지 추가
    - 오타 수정 test -> text

--- src/errors/NetworkError.ts
+++ src/errors/NetworkError.ts
@@ -1,6 +1,6 @@
 class NetworkError extends Error {
   code: number;
-  statusTest: string;
+  statusText: string;
   response: unknown;
 
   static errorMessages: Record<number, string> = {
@@ -8,6 +8,7 @@ class NetworkError extends Error {
     403: '접근 권한이 없습니다.',
     404: '요청한 리소스를 찾을 수 없습니다.',
     429: '너무 많은 요청을 보냈습니다. 잠시 후 다시 시도해주세요.',
+    422: '요청이 유효성 검사에 실패했습니다',
     500: '서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.',
     502: '잘못된 게이트웨이입니다.',
     503: '서버가 현재 이용 불가능합니다. 잠시 후 다시 시도해주세요.',
@@ -17,19 +18,19 @@ class NetworkError extends Error {
   constructor({
     code,
     message,
-    statusTest,
+    statusText,
     response,
   }: {
     code: number;
     message: string;
-    statusTest: string;
+    statusText: string;
     response?: unknown;
   }) {
     const errorMessage = NetworkError.errorMessages[code] || message;
     super(errorMessage);
     this.name = 'NetworkError';
     this.code = code;
-    this.statusTest = statusTest;
+    this.statusText = statusText;
     this.response = response;
   }
 
