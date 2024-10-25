513b0fce648602396427619999591bc3d7443edf
hyogeun <stonepagoda8@gmail.com>
2024-10-25 18:44:52 +0900

    feat: 개발, 배포에 따라 환경변수 설정
    - 개발, 배포에 따라 환경변수 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,23 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
-  console.log(env.VITE_API_URL);
-  return {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL, // https://api.realworld.io/api 를 사용
-          changeOrigin: true, // CORS 우회
-          rewrite: (path) => path.replace(/^\/api/, ''), // 경로 수정
-          secure: false, // HTTPS 인증서 문제 우회
+  // console.log(env.VITE_API_URL);
+  if (mode === 'development') {
+    return {
+      plugins: [react()],
+      server: {
+        proxy: {
+          '/api': {
+            target: env.VITE_API_URL, // https://api.realworld.io/api 를 사용
+            changeOrigin: true, // CORS 우회
+            rewrite: (path) => path.replace(/^\/api/, ''), // 경로 수정
+            secure: false, // HTTPS 인증서 문제 우회
+          },
         },
       },
-    },
+    };
+  }
+  return {
+    plugins: [react()],
   };
 });
