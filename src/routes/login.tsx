b46796b0f034353594469cf1c50578d15049b17c
hyogeun <stonepagoda8@gmail.com>
2024-10-28 23:03:29 +0900

    fix: 로그인 상태 저장
    - 로그인 하고 상태 저장을 안했었음

--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -3,11 +3,13 @@ import AuthForm from '../components/AuthForm';
 import { QueryClient } from '@tanstack/react-query';
 import NetworkError from '../errors/NetworkError';
 import { authQueryOptions } from '../queryOptions/authQueryOptions';
+import { useBoundStore } from '../store';
 
 export const action =
   (queryClient: QueryClient) =>
   async ({ request }: ActionFunctionArgs) => {
     const formData = await request.formData();
+    const loginFn = useBoundStore.getState().login;
     if (!formData.get('email') || !formData.get('password')) {
       return { errors: { 'email or password': ['All fields are required'] } };
     }
@@ -16,9 +18,11 @@ export const action =
     const password = formData.get('password') as string;
 
     try {
-      await queryClient.fetchQuery(
+      const response = await queryClient.fetchQuery(
         authQueryOptions.login({ user: { email, password } }),
       );
+      const { user } = response;
+      loginFn(user, user.token);
 
       return redirect('/');
     } catch (error) {
