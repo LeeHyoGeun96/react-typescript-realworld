9a8b0dc97b68777bf65d862cc3b457ba7581aaf4
hyogeun <stonepagoda8@gmail.com>
2024-10-28 22:59:39 +0900

    refactor: 회원가입 로직 개선
    - staleTime, gcTime 설정

--- src/queryOptions/authQueryOptions.ts
+++ src/queryOptions/authQueryOptions.ts
@@ -4,19 +4,25 @@ import { authService } from '../services/auth';
 export const authQueryOptions = {
   signup: (data: SignupRequest) =>
     queryOptions({
-      queryKey: ['signup'],
+      queryKey: ['auth', 'signup'],
       queryFn: () => authService.signUp(data),
+      staleTime: 0,
+      gcTime: 0,
     }),
 
   login: (data: LoginRequest) =>
     queryOptions({
-      queryKey: ['login'],
+      queryKey: ['auth', 'login'],
       queryFn: () => authService.login(data),
+      staleTime: 0,
+      gcTime: 0,
     }),
 
   getCurrentUser: (token: string) =>
     queryOptions({
-      queryKey: ['currentUser', token],
+      queryKey: ['auth', 'currentUser', token],
       queryFn: () => authService.getCurrentUser(token),
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
     }),
 };
